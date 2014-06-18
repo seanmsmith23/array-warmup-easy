8322faa07f4f9d726f5763d25dd1fd8782881245
Sean Smith <seansmith1020@gmail.com>
2014-06-18 11:05:46 -0600

    Dogs.rb that passes all checks

--- dogs.rb
+++ dogs.rb
@@ -3,36 +3,56 @@ require_relative "testing_library"
 dogs = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"]
 
 def how_many_dogs(dogs)
-
+  dogs.size
 end
 
 def name_lengths(dogs)
-
+  dogs.collect{ |dog| dog.length}
 end
 
 def reverse_dog_names(dogs)
-
+  dogs.collect{ |dog| dog.reverse}
 end
 
 def first_three_dogs_with_each(dogs)
-
+  empty = []
+  dogs.each{ |dog| empty << dog if dog < 'N' }
+  empty
 end
 
 def first_three_dogs_without_each(dogs)
-
+  dogs[0..2]
 end
 
 def reverse_case_dog_names(dogs)
-
+  dogs.collect{ |dog| dog.swapcase}
 end
 
 def sum_of_all_dog_name_lengths(dogs)
-
+  empty = []
+  dogs.each do |dog|
+    empty << dog.length
+  end
+  x = 0
+  number = 0
+  while x < empty.length
+    number += empty[x]
+    x += 1
+  end
+  number
 end
 
 def dogs_with_long_names(dogs)
-
+  empty = []
+  dogs.each do |dog|
+    if  dog.length > 4
+      empty << true
+    else empty << false
+    end
+  end
+  empty
 end
+
 
 puts "*"*80
 puts "Make each method return the correct value"
